970cb1ce33d5f43a2fccc0349e6cca5fddeed694
James McDonald <j4mesmcdonald@gmail.com>
2023-02-21 21:05:39 -0500

    base

--- bitbot_common/base.py
+++ bitbot_common/base.py
@@ -1,7 +1,7 @@
 def rezip(iterable, n=2, strict=True):
     iterable = tuple(iterable)
     if len(iterable):
-        return (zip_strict if strict else zip)(*iterable)
+        return zip(*iterable, strict=strict)
     if n:
         return [()] * n
     raise ValueError
