536c415167ffd12fcea809f25605aeed8ac8e5a9
James McDonald <j4mesmcdonald@gmail.com>
2023-02-21 19:46:23 -0500

    small

--- setup.py
+++ setup.py
@@ -2,17 +2,17 @@ from os.path import join, dirname
 
 from setuptools import setup
 
-__version__ = open(join(dirname(__file__), '_utils', '__version__')).read().strip()
+__version__ = open(join(dirname(__file__), "common", "__version__")).read().strip()
 
-install_requires = [line.strip() for line in open('requirements.txt', 'r').readlines()]
+install_requires = [line.strip() for line in open("requirements.txt", "r").readlines()]
 
 setup(
-    name='common',
+    name="common",
     version=__version__,
-    description='Bitbot Common package utils',
-    author='James McDonald',
-    author_email='j4mesmcdonald@gmail.com',
+    description="Bitbot Common package utils",
+    author="James McDonald",
+    author_email="j4mesmcdonald@gmail.com",
     install_requires=install_requires,
-    packages='common common.httpx common.persist'.split(),
-    include_package_data=True
+    packages="common common.httpx common.persist".split(),
+    include_package_data=True,
 )
